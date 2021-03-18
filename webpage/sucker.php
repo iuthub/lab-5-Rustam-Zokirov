cf683b86830257a9b842e85d5a3b2b150104ec5e
Rustam-Z <zokirovrustam202@gmail.com>
2021-03-18 21:53:01 +0500

    second part done

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -1,6 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="buyagrade.css" type="text/css" rel="stylesheet"/>
+    <title>Data Recieved</title>
+</head>
+<body>
+	<h1>Thanks, sucker!</h1>
+	
+	<p>Your information has been received.</p1>
+	
+	
+	<dl>
+		<dt>Name</dt>
+		<dd>
+			<?= $_REQUEST['name'] ?>
+		</dd>
+		
+		<dt>Section</dt>
+		<dd>
+			<?= $_REQUEST['section'] ?>
+		</dd>
+		
+		<dt>Credit Card</dt>
+		<dd>
+			<?= $_REQUEST['credit_card'] ?>
+			<?= $_REQUEST['credit_card_type'] ?>
+		<dd>
 
-
-<?php
-echo "My first PHdsfsdfsdfP script!";
-?>
-
+		</dd>
+	</dl>
+			
+</body>
+</html>
